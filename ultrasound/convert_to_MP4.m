b27a06154eb722484c921065a61412ce68c39a60
Tim travel <timvdzee@hotmail.com>
2026-03-11 08:53:29 +0100

    small update

--- ultrasound/convert_to_MP4.m
+++ ultrasound/convert_to_MP4.m
@@ -1,24 +1,27 @@
 clear all; close all; clc
 
+mainfolder = 'C:\Users\u0167448\OneDrive - KU Leuven\10. Energetics\0903\P3';
 mainfolder = 'C:\Users\u0167448\Documents\Data\SRS\Metingen ultrasounds';
 
 folders = dir(mainfolder);
 
-for i = 1:length(folders)-3
-    cd(fullfile(folders(i+2).folder, folders(i+2).name));
-    
-    TVDfiles = dir('*.tvd');
-   
-    for j = 1:length(TVDfiles)
-        TVDfile = fullfile(folders(i+2).folder, folders(i+2).name, TVDfiles(j).name);
-        
-        if ~exist(strrep(TVDfile,'.tvd','.mp4'), 'file')
-        
-            TVDdata = TVD2ALL(TVDfile,'VideoQuality', 100); 
+for i = 1:length(folders)-2
+    if folders(i+2).isdir
+        cd(fullfile(folders(i+2).folder, folders(i+2).name));
 
-            save(strrep(TVDfile,'.tvd','.mat'),'TVDdata')
-            disp(['Saved: ', TVDfile])
+        TVDfiles = dir('*.tvd');
+
+        for j = 1:length(TVDfiles)
+            TVDfile = fullfile(folders(i+2).folder, folders(i+2).name, TVDfiles(j).name);
+
+            if ~exist(strrep(TVDfile,'.tvd','.mp4'), 'file')
+
+                TVDdata = TVD2ALL(TVDfile,'VideoQuality', 100); 
+
+                save(strrep(TVDfile,'.tvd','.mat'),'TVDdata')
+                disp(['Saved: ', TVDfile])
+            end
+
         end
-        
     end
 end
